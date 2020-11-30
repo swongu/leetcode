ddddf5489d5482720b7b57c2f88e0c409758cd94
swongu <swongu@gmail.com>
2020-11-30 08:45:03 -0800

    DP solution

--- coin-change.java
+++ coin-change.java
@@ -3,47 +3,33 @@ import java.util.Arrays;
 class Solution
 {
    int[] coins;
+   int[] dp;
    int s = Integer.MAX_VALUE;
 
    public int coinChange(int[] coins, int amount)
    {
       this.coins = coins;
-      Arrays.sort(coins);
+      this.dp = new int[amount + 1];
+      Arrays.fill(dp, Integer.MAX_VALUE);
+      dp[0] = 0;
 
-      bt(coins.length - 1, 0, amount);
-      return s == Integer.MAX_VALUE ? -1 : s;
-   }
-
-   public void bt(int index, int used, int amount)
-   {
-      if (reject(index, used, amount))
+      for (int current = 1; current < amount + 1; ++current)
       {
-         return;
+         for (int coin: coins)
+         {
+            int previous = current - coin;
+            if (previous < 0) continue;
+            if (dp[previous] == -1) continue;
+
+            dp[current] = Math.min(dp[current], dp[previous] + 1);
+         }
+          
+         if (dp[current] == Integer.MAX_VALUE)
+         {
+            dp[current] = -1;
+         }
       }
-
-      if (accept(amount))
-      {
-         // System.out.println("Found answer using " + used + " coins");
-         s = Math.min(used, s);
-         return;
-      }
-
-      for (int i = amount / coins[index]; i >= 0 && used + i < s; --i)
-      {
-         // System.out.println("Using " + i + " " + coins[index] + "-coins, " + amount + " reduced to " + (amount - coins[index] * i));
-         used += i;
-         bt(index - 1, used, amount - coins[index] * i);
-         used -= i;
-      }
-   }
-
-   public boolean reject(int index, int used, int amount)
-   {
-      return (index < 0 && amount > 0) || used >= s;
-   }
-
-   public boolean accept(int amount)
-   {
-      return amount == 0;
+      
+      return dp[amount];
    }
 }
