88aa39b268fcd6626f64cf1f94e65bc4fcd3c625
swongu <swongu@gmail.com>
2020-12-06 20:51:50 -0800

    Update binary-tree-maximum-path-sum.java

--- binary-tree-maximum-path-sum.java
+++ binary-tree-maximum-path-sum.java
@@ -13,116 +13,28 @@
  *     }
  * }
  */
-class Solution 
+class Solution
 {
-    Map<TreeNode, Integer> map;
-    int n;
-    int[][] dp;
-    int remain;
-    int max = Integer.MIN_VALUE;
-    
     public int maxPathSum(TreeNode root) 
     {
-        this.map = buildMapping(root);
-        this.n = map.size();
-        this.dp = new int[map.size()][map.size()];
-        this.remain = n * n;
+        int leftSum = (root.left != null) ? maxPathSum(root.left) : Integer.MIN_VALUE;
+        int rightSum = (root.right != null) ? maxPathSum(root.right) : Integer.MIN_VALUE;
         
-        initialize(root);
-        
-        // Floyd-Warshall
-        for (int k = 0; k < n; ++k)
-        {
-            for (int i = 0; i < n; ++i)
-            {
-                if (dp[i][k] == Integer.MAX_VALUE)
-                {
-                    continue;
-                }
-                
-                for (int j = i; j < n; ++j)
-                {
-                    if (dp[i][j] != Integer.MAX_VALUE || dp[j][k] == Integer.MAX_VALUE)
-                    {
-                        continue;
-                    }
-                    
-                    // System.out.println("(i, j, k) = (" + i + ", " + j + ", " + k + "), " +
-                                       // "dp[i][k] + dp[k][j] - dp[k][k] = " +
-                                       // dp[i][k] + " + " + dp[k][j] + " - " + dp[k][k]);
-                    set(i, j, dp[i][k] + dp[k][j] - dp[k][k]);
-                    if (remain == 0)
-                    {
-                        // System.out.println("No more entries to fill");
-                        return max;
-                    }
-                }
-            }
-        }
-        
-        return max;
+        int leftGain = (root.left != null) ? gain(root.left) : 0;
+        int rightGain = (root.right != null) ? gain(root.right) : 0;
+
+        int sum_1 = leftSum;
+        int sum_2 = leftGain + root.val + rightGain;
+        int sum_3 = rightSum;
+        return Math.max(sum_1, Math.max(sum_2, sum_3));    
     }
     
-    public Map<TreeNode, Integer> buildMapping(TreeNode root)
+    public int gain(TreeNode root)
     {
-        Map<TreeNode, Integer> map = new HashMap<>();
-        int i = 0;
+        int left = (root.left != null) ? gain(root.left) : 0;
+        int right = (root.right != null) ? gain(root.right) : 0;
         
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty())
-        {
-            TreeNode node = queue.remove();
-            map.put(node, i++);
-            if (node.left != null)
-            {
-                queue.add(node.left);
-            }
-            
-            if (node.right != null)
-            {
-                queue.add(node.right);
-            }
-        }
-        
-        return map;
-    }
-    
-    public void initialize(TreeNode root)
-    {
-        for (int i = 0; i < n; ++i)
-        {
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
-        }
-        
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty())
-        {
-            TreeNode node = queue.remove();
-            int i = map.get(node);
-            set(i, i, node.val);
-            
-            if (node.left != null)
-            {
-                int j = map.get(node.left);
-                set(i, j, node.val + node.left.val);
-                queue.add(node.left);
-            }
-            
-            if (node.right != null)
-            {
-                int j = map.get(node.right);
-                set(i, j, node.val + node.right.val);
-                queue.add(node.right);
-            }
-        }
-    }
-    
-    public void set(int i, int j, int val)
-    {
-        dp[i][j] = dp[j][i] = val;
-        max = Math.max(max, val);
-        remain -= (i == j) ? 1 : 2;
+        int gain = Math.max(left + root.val, right + root.val);
+        return Math.max(0, gain);
     }
 }
